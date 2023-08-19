b0043f02b58d2cc2c00715857f1606d8ceb8422a
sijune <sijune0525@gmail.com>
2023-08-19 14:01:34 +0900

    api first lecture finish

--- src/main/java/jpabook/jpashop/domain/Item/Book.java
+++ src/main/java/jpabook/jpashop/domain/Item/Book.java
@@ -11,6 +11,6 @@ import javax.persistence.Entity;
 @Getter
 @Setter
 public class Book extends Item {
-    private String artist;
+    private String author;
     private String isbn;
 }
--- src/main/java/jpabook/jpashop/service/OrderService.java
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -1,10 +1,7 @@
 package jpabook.jpashop.service;
 
-import jpabook.jpashop.domain.Delivery;
+import jpabook.jpashop.domain.*;
 import jpabook.jpashop.domain.Item.Item;
-import jpabook.jpashop.domain.Member;
-import jpabook.jpashop.domain.Order;
-import jpabook.jpashop.domain.OrderItem;
 import jpabook.jpashop.repository.ItemRepository;
 import jpabook.jpashop.repository.MemberRepository;
 import jpabook.jpashop.repository.OrderRepository;
@@ -49,7 +46,7 @@ public class OrderService {
         order.cancel();
     }
 
-//    public List<Order> findOrders(OrderSearch orderSearch) {
-//        return orderRepository.findAll(orderSearch);
-//    }
+    public List<Order> findOrders(OrderSearch orderSearch) {
+        return orderRepository.findAllByCriteria(orderSearch);
+    }
 }
